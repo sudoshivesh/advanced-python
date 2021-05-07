783385e89c3ea3ad41f0fb597aa0300e74c538c9
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2021-05-07 10:33:49 +0530

    Create interval_display_using_pyautogui.py

---
+++ interval_display_using_pyautogui.py
@@ -0,0 +1,6 @@
+#It will display the output at a given interval
+#we will import pyautogui
+#to install pyautogui, you have to type, pip intsall pyautogui, in cmd or terminal
+
+import pyautogui
+pyautogui.typewrite("Welcome to github,It is being displayed automatically",interval=0.30)
